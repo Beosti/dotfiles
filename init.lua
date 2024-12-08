55010bffdc44a94ba26645985e73d7ba2ee70ee0
beosti <beosti@archlinux.localdomain>
2024-12-08 23:35:06 +0100

    updated the dotfile for nvim

--- init.lua
+++ init.lua
@@ -3,6 +3,9 @@ vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 
+vim.keymap.set('n', '<C-h>', ":Neotree filesystem reveal left<CR>", {})
+vim.keymap.set('n', '<C-e>', ':exit<CR>', {})
+
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   local lazyrepo = "https://github.com/folke/lazy.nvim.git"
@@ -20,10 +23,34 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 local plugins = {
-  {"catppuccin/nvim", name= "catppuccin", priority = 1000}
+  {"catppuccin/nvim", name= "catppuccin", priority = 1000},
+  {
+    'nvim-telescope/telescope.nvim', tag = '0.1.8',
+    dependencies = { 'nvim-lua/plenary.nvim' }
+  },
+  {
+    "nvim-treesitter/nvim-treesitter", build = ":TSUpdate"
+  },
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons",
+      "MunifTanjim/nui.nvim"
+    },
+  },
 }
 local opts = {}
-print("hello world test")
 require("lazy").setup(plugins, opts)
+local builtin = require("telescope.builtin")
+vim.keymap.set('n', '<C-p>', builtin.find_files, {})
 require("catppuccin").setup()
 vim.cmd.colorscheme "catppuccin"
+local config = require("nvim-treesitter.configs")
+config.setup({
+  ensure_installed = {"lua"},
+  highlight = { enable = true },
+  indent = { enable = true },
+})
+print("file sourced")
