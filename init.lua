41712dcb6a79231a5a41c19e8955d7a69c731fc3
beosti <beosti@archlinux.localdomain>
2024-12-10 22:29:58 +0100

    broke one big file in a more readable thing with multiple files

--- init.lua
+++ init.lua
@@ -22,35 +22,6 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-local plugins = {
-  {"catppuccin/nvim", name= "catppuccin", priority = 1000},
-  {
-    'nvim-telescope/telescope.nvim', tag = '0.1.8',
-    dependencies = { 'nvim-lua/plenary.nvim' }
-  },
-  {
-    "nvim-treesitter/nvim-treesitter", build = ":TSUpdate"
-  },
-  {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons",
-      "MunifTanjim/nui.nvim"
-    },
-  },
-}
 local opts = {}
-require("lazy").setup(plugins, opts)
-local builtin = require("telescope.builtin")
-vim.keymap.set('n', '<C-p>', builtin.find_files, {})
-require("catppuccin").setup()
-vim.cmd.colorscheme "catppuccin"
-local config = require("nvim-treesitter.configs")
-config.setup({
-  ensure_installed = {"lua"},
-  highlight = { enable = true },
-  indent = { enable = true },
-})
+require("lazy").setup("plugins")
 print("file sourced")
